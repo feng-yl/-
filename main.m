71c7667d6af7010ed4d3c99411055f6fc9e17310
linyf950@gmail.com <lyf>
2019-10-28 21:34:37 +0800

    第一次运行20191028

--- main.m
+++ main.m
@@ -1,6 +1,7 @@
 
-geo = zeros(1000,1);
-xyz = zeros(1000,1);
+geo = zeros(1000,3);
+res = zeros(1000,3);
+xyz = zeros(1000,3);
 [mag_field_vector,hor_intensity,declinatioon,inclination,total_intensity] ...
 = igrfmagm(1000,30.513111570341643,114.41821564648436,decyear(2019,10,31),12);
 xyz(1,:) = mag_field_vector;
@@ -15,3 +16,8 @@ end
 
 %%
 %2
+for i=2:1000
+    left = left_matr( xyz(i-1,:));
+    right = right_matr( xyz(i-1,:), xyz(i,:) );
+    res(i,:) = (pinv(left)*right)';
+end
