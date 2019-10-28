4f0ab9b9b8b298ee5f8170daf42296e297af2add
linyf950@gmail.com <lyf>
2019-10-28 15:35:11 +0800

    修改left_matr

--- left_matr.m
+++ left_matr.m
@@ -1,7 +1,7 @@
-function matr = left_matr( m )
+function left = left_matr( m )
 %UNTITLED4 此处显示有关此函数的摘要
 %   此处显示详细说明
-matr=[-m(2) -m(2) m(1)-m(3);
+left=[-m(2) -m(2) m(1)-m(3);
       m(1) m(3)-m(2) m(1);
       m(1)-m(2) m(3) -m(3)];
 
